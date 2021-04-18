4414ff5e44aacd83b5a3861d6302b4551a6333b8
Adrian Mullings <adralemechk@gmail.com>
2021-04-18 18:43:20 -0400

    Remove downvotes; add votes to 2e-feature-discussion

--- votes.py
+++ votes.py
@@ -7,25 +7,29 @@ import client
 import roles
 
 # Constants
-CHANNEL_ID = 816863262917394463  # 2e-beta-discussion
-# CHANNEL_ID = 257073315011624961  # dev-test
+CHANNEL_IDS = [
+    816863262917394463,  # 2e-beta-gameplay
+    833462332553560084,  # 2e-feature-discussion
+    #257073315011624961,  # dev-test
+]
 EXEMPT_USERS = {
     741181637244485652, # Liam
 }
 
 
 async def on_ready():
-    print('Adding up/downvote emoji to messages in channel #{0} ({1})'.format(
-        client.discordClient.get_channel(CHANNEL_ID),
-        CHANNEL_ID))
+    print('Adding upvote emoji to messages in the following channels:')
+    [
+        print('{0} ({1})'.format(client.discordClient.get_channel(id), id))
+        for id in CHANNEL_IDS
+    ]
 
 
 async def on_message(message: discord.Message):
-    if message.channel.id != CHANNEL_ID:
+    if message.channel.id in CHANNEL_IDS:
         return
 
     if message.author.id in EXEMPT_USERS:
         return
 
     await message.add_reaction('👍')
-    await message.add_reaction('👎')
