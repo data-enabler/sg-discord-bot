69b86ffa4bd794c2f5c8b3817003253ea0533df9
Adrian Mullings <adralemechk@gmail.com>
2021-04-18 18:54:33 -0400

    Limit gateway intents to only those necessary

--- client.py
+++ client.py
@@ -1,6 +1,10 @@
 import discord
 
-intents = discord.Intents.default()
-intents.typing = False
+intents = discord.Intents.none()
+intents.emojis = True
+intents.guild_messages = True
+intents.guild_reactions = True
+intents.guilds = True
+intents.members = True
 
 discordClient: discord.Client = discord.Client(intents=intents)
